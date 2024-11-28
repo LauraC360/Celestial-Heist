88e320d2c68cdc7a469596672f4b7b7c690f8a03
AndrewIGD <andreistoleru03@gmail.com>
2024-11-28 14:02:19 +0200

    Mid-term commit

---
+++ Assets/Scripts/ShipControl.cs
@@ -0,0 +1,12 @@
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ShipControl : XRGrabInteractable
+{
+    protected override void Grab()
+    {
+    }
+
+    protected override void Drop()
+    {
+    }
+}
